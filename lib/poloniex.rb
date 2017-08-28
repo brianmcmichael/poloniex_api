b9bffb274ddc4aca29ee0ec12b7a59f24ef53d79
Brian McMichael <brian@brianmcmichael.com>
2017-08-28 03:14:10 +0000

    return_balances...return_deposit_addresses

--- lib/poloniex.rb
+++ lib/poloniex.rb
@@ -1,6 +1,24 @@
 # Poloniex API wrapper for Ruby 2.1.5+
 # https://github.com/brianmcmichael/poloniex_api
 # BTC: 1Azh1Sn3nzHE5RMnx8dQnJ4dkWAxFbUWEg
+# ETH/Tokens: 0x7e2185544f095230ba86915E5BfF498653A90A66
+# Waves: 3P6FDphVYMEgy6ts4o8HLgRDUKoyRnMDJ9y
+#
+#    Copyright (C) 2017  https://github.com/brianmcmichael
+#
+#    This program is free software; you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation; either version 2 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License along
+#    with this program; if not, write to the Free Software Foundation, Inc.,
+#    51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 
 require 'net/http'
 require 'json'
@@ -176,7 +194,7 @@ module Poloniex
 
         # update nonce if we fell behind
         if out['error'].include? "Nonce must be greater"
-          self._nonce = int(out['error'].split('.')[0].split()[-1])
+          nonce
           # raise RequestException so we try again
           raise RequestException("PoloniexError #{out['error']}")
         end
@@ -296,14 +314,17 @@ module Poloniex
     end
 
     # Returns all of your deposit addresses.
-    def return_depost_addresses
+    def return_deposit_addresses
       return self.call('returnDepositAddresses')
     end
 
     # Generates a new deposit address for the currency specified by the
     #   "currency" parameter.
     def generate_new_address(currency)
-      return self.call('generateNewAddress', { 'currency' => currency })
+      args = {
+          'currency' => currency
+      }
+      return self.call('generateNewAddress', args)
     end
 
     # Returns your deposit and withdrawal history within a range,
